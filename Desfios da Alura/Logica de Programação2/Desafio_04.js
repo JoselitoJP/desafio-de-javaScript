a5569fcec85ad443b10a6efef2233cbaff236c8e
JoãoPaulo <138796377+JoselitoJP@users.noreply.github.com>
2023-11-21 12:31:16 -0300

    Add files via upload

--- Desfios da Alura/Logica de Programação2/Desafio_04.js
+++ Desfios da Alura/Logica de Programação2/Desafio_04.js
@@ -9,8 +9,39 @@ alert('Você está feliz');
  var listaGenerica = [];
  
  /*
-  02-Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 
-  'JavaScript','C','C++', 'Kotlin' e 'Python'
+	02-Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 
+	'JavaScript','C','C++', 'Kotlin' e 'Python'
  */
+ 
+ var linguagensDeProgramacao = ['JavaScript' , 'C' , 'C++' , 'Kotlin' , 'Python'];
+ 
+ console.log(linguagensDeProgramacao.length);
+ /*
+	03-Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+ */
+ 
+ linguagensDeProgramacao.push('Java', 'Ruby','GoLang');
+ 
+ console.log(linguagensDeProgramacao.length);
+ 
+ /*
+	04-Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+ */
+ 
+ var nome = ['joao', 'Paulo' , 'Daniel'];
+ 
+ console.log(nome[0]);
+ 
+ /*
+	05-Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+ */
+ 
+ console.log(nome[1]);
+  
+ /*
+	06-Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+ */
+ 
+ console.log(nome[2]);
  
  
